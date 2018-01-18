10a33cc6434a9fd55fd1ba0eac2bb754de13e0e3
sbguneri <sburakguneri@gmail.com>
2018-01-18 08:25:33 +0300

    getter & setter methods added

--- src/main/java/com/demo/domain/model/Address.java
+++ src/main/java/com/demo/domain/model/Address.java
@@ -2,7 +2,31 @@ package com.demo.domain.model;
 
 public class Address {
 
-    public int id;
-    public String city;
-    public String fullAddress;
+    private int id;
+    private String city;
+    private String fullAddress;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getFullAddress() {
+		return fullAddress;
+	}
+
+	public void setFullAddress(String fullAddress) {
+		this.fullAddress = fullAddress;
+	}
 }
