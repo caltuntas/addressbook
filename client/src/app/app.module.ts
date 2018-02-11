539c70ae2bf4e58fd239317b96758d0408d8b17b
cihataltuntas <btabcdef>
2018-02-11 22:47:47 +0300

    Angular component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ProductComponent } from './product/product.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactFormComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/contact.ts
@@ -0,0 +1,10 @@
+export class Contact {
+  constructor(
+    public id:number,
+    public name:string,
+    public address:string,
+    public phone:string
+  )
+  {
+  }
+}
